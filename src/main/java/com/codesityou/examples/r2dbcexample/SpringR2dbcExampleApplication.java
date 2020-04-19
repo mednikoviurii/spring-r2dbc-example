b11d4a5794d74164a759bf9446528da3c7507a3e
Iurii Mednikov <yuri.mednikov@mednikov.net>
2020-04-19 17:34:12 +0200

    Initial commit

---
+++ src/main/java/com/codesityou/examples/r2dbcexample/SpringR2dbcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.codesityou.examples.r2dbcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringR2dbcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringR2dbcExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesityou/examples/r2dbcexample/SpringR2dbcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesityou.examples.r2dbcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringR2dbcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
